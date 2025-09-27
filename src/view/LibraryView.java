5941fa760bbaefb54d43144e4a0e10a72fa11f7b
okamikira <ookamikiraa@gmail.com>
2025-09-27 17:02:47 +0200

    - Formatted with intelJ formatter

--- src/view/LibraryView.java
+++ src/view/LibraryView.java
@@ -1,24 +1,25 @@
-    package view;
+package view;
 
-    import model.Book;
-    import java.util.Collection;
+import model.Book;
 
-    public class LibraryView {
+import java.util.Collection;
 
-        public static String showBooks(Collection<Book> books) {
-            if (books.isEmpty()) {
-                return "Brak książek spełniających kryteria.";
-            }
-            StringBuilder sb = new StringBuilder();
-            int index = 1;
+public class LibraryView {
 
-            for (Book book : books) {
-                sb.append(index)
-                        .append(". ")
-                        .append(BookView.showBook(book))
-                        .append("\n");
-                index++;
-            }
-            return sb.toString();
+    public static String showBooks(Collection<Book> books) {
+        if (books.isEmpty()) {
+            return "Brak książek spełniających kryteria.";
         }
+        StringBuilder sb = new StringBuilder();
+        int index = 1;
+
+        for (Book book : books) {
+            sb.append(index)
+                    .append(". ")
+                    .append(BookView.showBook(book))
+                    .append("\n");
+            index++;
+        }
+        return sb.toString();
     }
+}
